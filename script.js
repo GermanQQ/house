b402eecf03591bb6d7cf2115794d583fcb43299d
German <GermanQQ@github.com>
2020-05-19 13:18:35 +0300

    Добавил страницу intro

---
+++ script.js
@@ -0,0 +1,24 @@
+
+function bg(){
+background = document.querySelector('.moon');
+background.style.transform = 'translateY(0%)';
+document.querySelector('.full').style.transform = 'translateY(0%)';
+document.querySelector('.left').style.transform = 'translateX(0%)';
+document.querySelector('.up').style.transform = 'translateX(0%)';
+document.querySelector('.house').style.transform = 'translateX(0%)';
+document.querySelector('.house').style.opacity = '1';
+document.querySelector('.intro').style.opacity ='1';
+}
+bg();
+
+function overflow(){
+    document.querySelector('body').style.overflow = 'auto';
+}
+setTimeout(overflow, 5000);
+
+function search(){
+        document.querySelector('.input__search').style.width = '100%';
+        document.querySelector('.input__search').style.cursor = 'text';
+        document.querySelector('.input__search').style.opacity = '1';
+
+}
