aab4b5b2b298fc2e101fa74c78f526c05b1f1771
German <GermanQQ@github.com>
2020-05-28 13:51:06 +0300

    Попровил ошибки в html

--- script.js
+++ script.js
@@ -9,13 +9,14 @@ document.querySelector('.up').style.transform = 'translateX(0%)';
 document.querySelector('.house').style.transform = 'translateX(0%)';
 document.querySelector('.house').style.opacity = '1';
 document.querySelector('.intro').style.opacity ='1';
+document.querySelector('header').style.opacity = '1';
 }
 bg();
 
-function overflow(){
-    document.querySelector('body').style.overflow = 'auto';
-}
-setTimeout(overflow, 5000);
+// function overflow(){
+//     document.querySelector('body').style.overflow = 'auto';
+// }
+// setTimeout(overflow, 5000);
 
 function search(){
         document.querySelector('.input__search').style.width = '100%';
