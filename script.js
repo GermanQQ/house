193aa51a7f67af350d366d458e25fb601da43168
German <GermanQQ@github.com>
2020-05-31 01:30:31 +0300

    slowScroll

--- script.js
+++ script.js
@@ -29,19 +29,6 @@ function search(){
 
 //Якорные ссылки js
 
-// const anchors = document.querySelectorAll('a[href*="#"]'); 
-
-// for (let anchor of anchors){
-//     anchor.addEventListener("click", function(event){
-//         event.preventDefault();
-//         const blockID = anchor.getAttribute('href');
-//         document.querySelector('' + blockID).scrollIntoView({
-//             behavior: "smooth",
-//             block: "start"
-//         })
-//     })
-// }
-
 function slowScroll(id) {
     var offset = 0;
     $('html, body').animate({
